9f30ad4dc4bbe5b4e5367b841e7e76dbc5bcc635
RogerRosell <r@roger-rossell.com>
2024-10-16 00:51:12 +0100

    data fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,35 +6,37 @@ import AppInitialiser from '@/lib/AppInitialiser';
 import { getImagesList, getUniqueKeywords, getFilterData } from '@/lib/filterUtils';
 
 export default async function Home() {
+  let initTree;
   try {
-  const initTree = await getFullGalleryList();
-  const images = initTree && initTree.length > 0 && getImagesList(initTree);
-  const keywords = initTree && initTree.length > 0 && getUniqueKeywords(initTree);
+    initTree = await getFullGalleryList();
+  } catch (error) {
+    console.log("Error >> ", error);
+    return <main><p>There&apos;s been an error</p></main>
 
-  const filterData = images ? getFilterData(images) : undefined;
+  } finally {
+    const images = initTree && initTree.length > 0 && getImagesList(initTree);
+    const keywords = initTree && initTree.length > 0 && getUniqueKeywords(initTree);
+    const filterData = images ? getFilterData(images) : undefined;
 
-  return (
-    <main>
-      {keywords && keywords.length > 0 && images && images.length > 0 && initTree && (
-        <AppInitialiser
-          initTree={initTree}
-          filterData={filterData}
-          keywords={keywords}
-          allImages={images}
-          filteredImages={images}
-        >
-          <div>
-            <div className="flex gap-4">
-              <Filters />
+    return (
+      <main>
+        {keywords && keywords.length > 0 && images && images.length > 0 && initTree && (
+          <AppInitialiser
+            initTree={initTree}
+            filterData={filterData}
+            keywords={keywords}
+            allImages={images}
+            filteredImages={images}
+          >
+            <div>
+              <div className="flex gap-4">
+                <Filters />
+              </div>
+              <Gallery />
             </div>
-            <Gallery />
-          </div>
-        </AppInitialiser>
-      )}
-    </main>
-  )
-} catch (error) {
-  console.log("Error >> ", error);
-  return <main><p>There&apos;s been an error</p></main>
-}
+          </AppInitialiser>
+        )}
+      </main>
+    )
+  }
 }

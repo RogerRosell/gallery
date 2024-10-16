355772cf1201e8a63cd7ae79c7770043fd35aed1
RogerRosell <r@roger-rossell.com>
2024-10-16 01:36:43 +0100

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,11 +9,7 @@ export default async function Home() {
   let initTree;
   try {
     initTree = await getFullGalleryList();
-  } catch (error) {
-    console.log("Error >> ", error);
-    return <main><p>There&apos;s been an error</p></main>
-
-  } finally {
+ 
     const images = initTree && initTree.length > 0 && getImagesList(initTree);
     const keywords = initTree && initTree.length > 0 && getUniqueKeywords(initTree);
     const filterData = images ? getFilterData(images) : undefined;
@@ -38,5 +34,8 @@ export default async function Home() {
         )}
       </main>
     )
+  } catch (error) {
+    console.log("Error >> ", error);
+    return <main><p>There&apos;s been an error</p></main>
   }
 }

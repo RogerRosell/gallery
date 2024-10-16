4732e05003243d9d4d86068673a097412f4c85c9
RogerRosell <r@roger-rossell.com>
2024-10-16 07:40:29 +0100

    added auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,30 @@
 import type { Metadata } from "next";
 import "./globals.css";
-// import { getServerSession } from 'next-auth';
-// import SessionProvider from '@/components/SessionProvider';
+import { getServerSession } from 'next-auth';
+import SessionProvider from '@/components/SessionProvider';
+import NavMenu from '@/components/NavMenu';
 
 export const metadata: Metadata = {
   title: "Family Gallery",
   description: "Galeria de fotos de la familia",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
 
-  // const session = await getServerSession();
+  const session = await getServerSession();
   return (
     <html lang="en">
       <body className="font-sans">
-        {/* <SessionProvider session={session}> */}
+        <SessionProvider session={session}>
           <main className="p-32">
-            {/* <NavMenu /> */}
+            <NavMenu />
             {children}
           </main>          
-          {/* </SessionProvider> */}
+          </SessionProvider>
       </body>
     </html>
   );

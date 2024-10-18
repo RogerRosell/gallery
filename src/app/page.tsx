fc289e9e90869acdf93ef4210ae0556f10fe3938
RogerRosell <r@roger-rossell.com>
2024-10-18 20:14:06 +0100

    console.logs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,15 +8,20 @@ import { getImagesList, getUniqueKeywords, getFilterData } from '@/lib/filterUti
 export default async function Home() {
   try {
     const initTree = await getFullGalleryList();
+    console.log("initTree >> ", initTree);
  
     const images = initTree && initTree.length > 0 && getImagesList(initTree);
     const keywords = initTree && initTree.length > 0 && getUniqueKeywords(initTree);
     const filterData = images ? getFilterData(images) : undefined;
 
+    console.log("images >> ", images);
+    console.log("keywords >> ", keywords);
+    console.log("filterData >> ", filterData);
+
     return (
       <main>
         here comes the fun
-        {keywords && keywords.length > 0 && images && images.length > 0 && initTree && (
+        {keywords && keywords.length > 0 && images && images.length > 0 && initTree && filterData && (
           <AppInitialiser
             initTree={initTree}
             filterData={filterData}
